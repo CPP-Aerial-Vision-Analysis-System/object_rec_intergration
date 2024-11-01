6c9ef31b51c522fda7e836216c4f61212fac6f12
dkingryan <ryanttran315@gmail.com>
2024-11-01 22:53:23 +0000

    launch file

--- catkin_ws/build/video_cam/CTestTestfile.cmake
+++ catkin_ws/build/video_cam/CTestTestfile.cmake
@@ -1,8 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /video_camera/catkin_ws/src
-# Build directory: /video_camera/catkin_ws/build
+# Source directory: /video_camera/catkin_ws/src/video_cam
+# Build directory: /video_camera/catkin_ws/build/video_cam
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("video_cam")
